d22ac61571520dc2fde4743068644b48c08801ae
Abhishek32971 <abhishekrp32971@gmail.com>
2024-06-12 22:40:46 +0530

    Add project files.

---
+++ JokesApp/Data/Migrations/20240612162644_initialsetup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokesApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "JokeQuestion",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JokeAnswer",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "JokeQuestion",
+                table: "Joke",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "JokeAnswer",
+                table: "Joke",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ JokesApp/Models/Joke.cs
@@ -0,0 +1,13 @@
+﻿namespace JokesApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public String JokeQuestion { get; set; }
+        public String JokeAnswer { get; set; }
+        public Joke()
+        {
+            
+        }
+    }
+}
